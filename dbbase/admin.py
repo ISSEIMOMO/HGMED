5dda866a4265ed1df6fe04b6ea3c0e2ef7860325
Issei momonge <mggyggf@gmail.com>
2024-10-11 21:28:36 -0300

    first commit

--- dbbase/admin.py
+++ dbbase/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from dbbase.models import (Funcionario,Cargo,Departamento,Venda,Produto,Fornecedor,Carregamento,Desconto,
+                           Perda, Estoque, Despesa, Atividade, Ponto)
+
 # Register your models here.
+
+
+admin.site.register(Funcionario)
+admin.site.register(Cargo)
+admin.site.register(Departamento)
+admin.site.register(Venda)
+admin.site.register(Produto)
+admin.site.register(Fornecedor)
+admin.site.register(Carregamento)
+admin.site.register(Desconto)
+admin.site.register(Perda)
+admin.site.register(Estoque)
+admin.site.register(Despesa)
+admin.site.register(Atividade)
