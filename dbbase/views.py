572c8acee4bf176d25f2e4329442d3fb7527c95d
Issei momonge <mggyggf@gmail.com>
2024-10-12 14:42:27 -0300

    first commit

--- dbbase/views.py
+++ dbbase/views.py
@@ -12,7 +12,7 @@ def terminal(request):
     cargo = chamada(ve)
     if not cargo[0]:
         cargo = [True, "", ""]
-    contex={"re":cargo[1],"ch":cargo[2],"ve":ve,"url":request.build_absolute_uri()}
+    contex={"re":cargo[1],"ch":cargo[2],"ve":ve,"url":f"{request.build_absolute_uri()}excel/{ve.replace('\n','')}"}
     return render(request,"db/terminal.html", contex)
 
 
